1a9c088e4fbad4e58dcb2918df9e4129cd8a61d6
rcky1100 <rcky1100@gmail.com>
2026-09-19 18:13:07 +0530

    setup eslint and prettier

---
+++ frontend/eslint.config.js
@@ -0,0 +1,28 @@
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx}'],
+
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+
+  prettier,
+);
